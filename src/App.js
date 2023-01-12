1b1fdef1c9c959a30280c6f03b66b2164898f2ba
Benjamin Nordin <benjamin.nordin@sprinta.se>
2023-01-12 14:13:55 +0100

    Initial Commit Started Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { SocialList } from './components/home_socialLinks';
+import { NavigationBar } from './components/header_navbar';
+import { Introduction } from './components/home_introduction';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <>
+   <div className='wrapper'>
+    <NavigationBar/>
+    <Introduction></Introduction>
+    <SocialList />
+   </div>
+   </>
+   );
 }
 
 export default App;
