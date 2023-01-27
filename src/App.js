57bf6f6ef399846d08af308efa5fad74bbea0b21
Benjamin <godiset@gmail.com>
2023-01-27 16:21:18 +0100

    Removed unused variable messing with netlify deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { SocialList } from './components/home_socialLinks';
 import { MobileNavigationBar, NavigationBar } from './components/header_navbar';
